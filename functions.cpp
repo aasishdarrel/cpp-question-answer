c6a52dfc7e81658fa4163940d1ec49a94731fa91
Aasish Dairel <aasishdairel@gmail.com>
2025-10-08 23:38:01 +0530

    added on oct 8

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void callfun() {
+    cout<<"hi from the funtion !"<<endl;
+
+}
+
+
+
+int main() {
+    cout<<"before calling the function "<<endl;
+    callfun();
+    callfun();
+    cout<<"after calling the function ";
+
+
+
+}
